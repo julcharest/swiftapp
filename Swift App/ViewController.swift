b2fa9649896a7db273c5a7f581161a64eebf49c7
Julien Charest <julien.charest@imp.ac.at>
2018-01-24 11:06:54 +0100

    My First Commit

--- Swift App/ViewController.swift
+++ Swift App/ViewController.swift
@@ -10,9 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var theLabel: UILabel!
+    
+    var tapCount = 0
+    var birthYear = 1800
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        theLabel.text = "Hello there!"
+        tapCount = tapCount + 1
+        print("Button tapped", tapCount, "times!")
+        
+        if tapCount >= 10 {
+            theLabel.text = "You tapped the button 10 times"
+        }
+    }
+    @IBAction func Button2(_ sender: Any) {
+        theLabel.text = "Buttons are cool!"
+        if birthYear >= 2000 {
+            print ("You were born in the 21st Century")
+        }
+        if birthYear < 2000 {
+            if birthYear >= 1900 {
+                print ("You were born in the 20th Century")
+            } else {
+                print ("Error, cannot deal")
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor.red
     }
 
     override func didReceiveMemoryWarning() {
